73c962ae20781f1fd0afa45e99cc13483c4354a5
Al C <albert.r.carter@gmail.com>
2013-07-11 16:49:34 -0400

    Add visitor detail. Trouble with static files.

--- forms/views.py
+++ forms/views.py
@@ -1,7 +1,8 @@
 # Create your views here.
 from django.http import HttpResponse
 from forms.models import Visitor
-from django.template import Context, loader
+from django.shortcuts import render_to_response, get_object_or_404
+
 
 # def index(request):
 # 	return HttpResponse("Hello, world. You're at the index")
@@ -19,8 +20,9 @@ def visitor(request, visitor_id):
 	# return HttpResponse("You're at the page for visitor id %s's forms" %visitor_id)
 
 	latest_visitor_list = Visitor.objects.all()
-	t = loader.get_template('forms/visitorPage.html')
-	c = Context({
-		'latest_visitor_list': latest_visitor_list
-		})
-	return HttpResponse(t.render(c))
+	visitor = get_object_or_404(Visitor, pk=visitor_id)
+
+	return render_to_response('forms/visitorPage.html', {'latest_visitor_list': latest_visitor_list, 'visitor': visitor})
+#  
+
+
