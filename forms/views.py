7e3b9d8c09e0771fc7afc2b57664cb65c2aa5a7e
Al C <albert.r.carter@gmail.com>
2013-07-12 15:38:03 -0400

    Static files. Calendar viewable, not editable.

--- forms/views.py
+++ forms/views.py
@@ -1,8 +1,8 @@
 # Create your views here.
 from django.http import HttpResponse
 from forms.models import Visitor
-from django.shortcuts import render_to_response, get_object_or_404
-
+# from django.template import Context, loader
+from django.shortcuts import render_to_response
 
 # def index(request):
 # 	return HttpResponse("Hello, world. You're at the index")
@@ -20,9 +20,10 @@ def visitor(request, visitor_id):
 	# return HttpResponse("You're at the page for visitor id %s's forms" %visitor_id)
 
 	latest_visitor_list = Visitor.objects.all()
-	visitor = get_object_or_404(Visitor, pk=visitor_id)
-
-	return render_to_response('forms/visitorPage.html', {'latest_visitor_list': latest_visitor_list, 'visitor': visitor})
-#  
-
-
+	visitor = Visitor.objects.get(pk=visitor_id)
+	return render_to_response ('forms/visitorPage.html', {'latest_visitor_list': latest_visitor_list, 'visitor':visitor})
+	# t = loader.get_template('forms/visitorPage.html')
+	# c = Context({
+	# 	'latest_visitor_list': latest_visitor_list
+	# 	})
+	# return HttpResponse(t.render(c))
