c1967e82334b24fb2d9e8330bf1154f1fa3bc536
Al C <albert.r.carter@gmail.com>
2013-07-12 16:26:16 -0400

    work on serializing events to json. not working.

--- forms/views.py
+++ forms/views.py
@@ -1,8 +1,13 @@
 # Create your views here.
 from django.http import HttpResponse
-from forms.models import Visitor
+from forms.models import Visitor, Event
 # from django.template import Context, loader
 from django.shortcuts import render_to_response
+
+# serialize events for calendar
+from django.utils import simplejson
+from django.core.serializers.json import DjangoJSONEncoder
+
 
 # def index(request):
 # 	return HttpResponse("Hello, world. You're at the index")
@@ -21,6 +26,14 @@ def visitor(request, visitor_id):
 
 	latest_visitor_list = Visitor.objects.all()
 	visitor = Visitor.objects.get(pk=visitor_id)
+
+
+	# events = Event.objects.filter(
+		start__gte=start, end=request.end, title= request.title).values('visitor', 'start', 'end', 'title')
+	# data = simplejson.dumps(list(events), cls=DjangoJSONEncoder)
+	# 
+
+
 	return render_to_response ('forms/visitorPage.html', {'latest_visitor_list': latest_visitor_list, 'visitor':visitor})
 	# t = loader.get_template('forms/visitorPage.html')
 	# c = Context({
